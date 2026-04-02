8671939d7eefc822ee87bcddd843ef1ceac4149c
Luis A. Moreno <morenoluis458@gmail.com>
2026-04-02 10:44:49 -0700

    move firebase config to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZYZANCqnJdLEZ-KWJfCwMbLMTtn2YsBw",
-  authDomain: "luis-board-7fce3.firebaseapp.com",
-  projectId: "luis-board-7fce3",
-  storageBucket: "luis-board-7fce3.firebasestorage.app",
-  messagingSenderId: "240713125754",
-  appId: "1:240713125754:web:96ac6c06f6550fc713d0c3",
-  measurementId: "G-PG237EKQYJ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
